2e21530c7d3efd5e5bf6845f39488c6579c75811
Yang Fan <stoneyang0915@gmail.com>
2015-05-08 17:33:53 +0800

    ENH: edited font

--- DrawTMQI.m
+++ DrawTMQI.m
@@ -1,15 +1,19 @@
 clf;
 
+x = 1:1:8;
+Q = [0.8292 .8355 .8974 .8191 .8203 .7773 .8161 .9146];
+S = [.8801 .8292 .8651 .8844 .8855 .8591 .8776 .8637];
+N = [.1785 .2704 .5504 .1320 .1357 .0198 .1272 .6615];
+
 plot(x, Q, '-o', 'linewidth', 2);
 hold all;
 plot(x, S, '-+', 'linewidth', 2);
 hold all;
 plot(x, N, '-*', 'linewidth', 2);
-hold all;
 
-xlabel('TMO Index');
-hold all;
-ylabel('Scores');
+xlabel('TMO Index', 'FontSize', 14);
+ylabel('Scores', 'FontSize', 14);
+title('Tone mapped image quality assessment (trafficLight)', 'FontSize', 18);
 
 legend('Q', 'S', 'N'); 
 set(gca,'XTick',1:8);
