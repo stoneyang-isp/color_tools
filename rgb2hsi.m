6b5a623e324449929f3b13b2d84843c690b6c444
Yang Fan <stoneyang0915@gmail.com>
2015-06-09 11:03:37 +0800

    ENH: edited important bug

--- rgb2hsi.m
+++ rgb2hsi.m
@@ -22,18 +22,16 @@ r = rgb(:,:,1);
 g = rgb(:,:,2);
 b = rgb(:,:,3);
 
-[rows, cols, layers] = size(rgb);
-eps = 1e-12;
+% eps = 1e-12;
 
 % compute hue
 theta_num = 0.5 * ((r - g) + (r - b));
 theta_den = sqrt((r - g) .^ 2 + (r - b) .* (g - b));
-theta_den(theta_den == 0) = eps;
-theta = acos(theta_num ./ theta_den);
+theta = acos(theta_num ./ (theta_den + eps));
 
 h = theta;
-h(g > b) = 2 * pi - h(g > b);
-h = h / (2 * pi);
+h(b > g) = 2*pi - h(b > g);
+h = h / (2*pi);
 
 % compute saturation
 s_num = 3 * min(min(r, g), b);
@@ -46,7 +44,7 @@ h(s == 0) = 0;
 i = (r + g + b) / 3;
 
 % combine h, s, and i
-hsi = zeros(rows, cols, layers);
+hsi = zeros(size(rgb));
 hsi(:,:,1) = h;
 hsi(:,:,2) = s;
 hsi(:,:,3) = i;
